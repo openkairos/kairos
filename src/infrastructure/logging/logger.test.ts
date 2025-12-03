4e1ac0732aca3872c1437303266f8897aedcd3c2
Dhemy <imdhemy@gmail.com>
2025-12-03 01:21:18 +0100

    chore(test): enhance logger tests with dynamic log levels

--- src/infrastructure/logging/logger.test.ts
+++ src/infrastructure/logging/logger.test.ts
@@ -1,17 +1,24 @@
 import { sink } from 'pino-test';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { createLogger } from './logger';
 
 describe('Logger', () => {
-  it('should log message, context and log level', async () => {
+  test.each([
+    { level: 'fatal' },
+    { level: 'error' },
+    { level: 'warn' },
+    { level: 'info' },
+    { level: 'debug' },
+    { level: 'trace' },
+  ])('Log level: $level', async ({ level }) => {
     const stream = sink();
-    const logger = createLogger({ level: 'info' }, stream);
+    const logger = createLogger({ level: 'trace' }, stream);
 
-    logger.info('test message', { foo: 'bar' });
+    logger[level as keyof typeof logger]('test message', { foo: 'bar' });
 
     const logged = await stream.read();
     expect(logged.msg).toBe('test message');
-    expect(logged.level).toBe('info');
+    expect(logged.level).toBe(level);
     expect(logged.foo).toBe('bar');
   });
 });
