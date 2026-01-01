2e0e4a8586d4894e4679318b0adc3b4efb77592f
Dhemy <imdhemy@gmail.com>
2026-01-01 17:05:40 +0100

    chore(deps): bump @koala-ts/framework to 2.7.0 and update devDependencies

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,12 +1,9 @@
 import { type KoalaConfig } from '@koala-ts/framework';
-import { eventBusMiddleware } from '@koala-ts/framework/dist/Event';
 import { HomeController } from '@/controller/HomeController';
 
 export const appConfig: KoalaConfig = {
   controllers: [
     HomeController,
   ],
-  globalMiddleware: [
-    eventBusMiddleware,
-  ],
+  globalMiddleware: [],
 };
