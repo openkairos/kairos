04e650bbb79dfc610b4e41b08c467a45efbce4e3
Dhemy <imdhemy@gmail.com>
2026-01-25 14:11:20 +0100

    chore(security): pass user instead of authenticated user

--- src/shared/infrastructure/security/authentication/generate-access-token.test.ts
+++ src/shared/infrastructure/security/authentication/generate-access-token.test.ts
@@ -1,16 +1,15 @@
 import { describe, expect, test, vi } from 'vitest';
-import { type AuthenticatedUser } from '@/shared/application/security';
 import { generateAccessToken } from '@/shared/infrastructure/security/authentication/generate-access-token';
 
 describe('Generate access token', () => {
   test('generate access token', async () => {
-    const authenticatedUser = { user: { id: 'first-user-id', username: 'john' } };
+    const user = { id: 'first-user-id', username: 'john' };
     const jwtSigner = vi.fn();
 
-    const accessToken = await generateAccessToken(authenticatedUser as unknown as AuthenticatedUser, jwtSigner);
+    const accessToken = await generateAccessToken(user, jwtSigner);
 
     expect(accessToken.token_type).toBe('Bearer');
     expect(accessToken.expires_in).toBe(3600);
-    expect(jwtSigner).toHaveBeenCalledWith(authenticatedUser.user);
+    expect(jwtSigner).toHaveBeenCalledWith(user);
   });
 });
--- src/shared/infrastructure/security/authentication/generate-access-token.ts
+++ src/shared/infrastructure/security/authentication/generate-access-token.ts
@@ -1,12 +1,10 @@
 import { securityConfig } from '@/config';
-import { type AccessToken, type AuthenticatedUser } from '@/shared/application/security';
+import { type AccessToken } from '@/shared/application/security';
+import { type User } from '@/shared/domain';
 import { sign } from '@/shared/infrastructure/security/authentication/jose-jwt';
 import { type JwtSigner } from '@/shared/infrastructure/security/authentication/types';
 
-export async function generateAccessToken(
-  { user }: AuthenticatedUser,
-  signJwt: JwtSigner = sign,
-): Promise<AccessToken> {
+export async function generateAccessToken(user: User, signJwt: JwtSigner = sign): Promise<AccessToken> {
   return {
     token_type: 'Bearer',
     expires_in: securityConfig.accessToken.ttl,
