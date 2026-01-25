afcb9b733bf7f1b9b4d13c97371ef82ebb70a6b0
Dhemy <imdhemy@gmail.com>
2026-01-25 14:19:25 +0100

    chore(security): assert generated access token

--- src/shared/infrastructure/security/authentication/generate-access-token.test.ts
+++ src/shared/infrastructure/security/authentication/generate-access-token.test.ts
@@ -4,12 +4,13 @@ import { generateAccessToken } from '@/shared/infrastructure/security/authentica
 describe('Generate access token', () => {
   test('generate access token', async () => {
     const user = { id: 'first-user-id', username: 'john' };
-    const jwtSigner = vi.fn();
+    const jwtSigner = vi.fn().mockResolvedValue('signed-jwt-token');
 
     const accessToken = await generateAccessToken(user, jwtSigner);
 
     expect(accessToken.token_type).toBe('Bearer');
     expect(accessToken.expires_in).toBe(3600);
     expect(jwtSigner).toHaveBeenCalledWith(user);
+    expect(accessToken.access_token).toBe('signed-jwt-token');
   });
 });
