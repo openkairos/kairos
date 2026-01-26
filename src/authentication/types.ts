35b20e8cab9690e1a7ad60435bfbaae68a981ed8
Dhemy <imdhemy@gmail.com>
2026-01-26 10:32:33 +0100

    Update types.ts

--- src/authentication/types.ts
+++ src/authentication/types.ts
@@ -1,14 +1,4 @@
 import { type HttpRequest } from '@koala-ts/framework';
-import { type User } from '@/shared/domain';
-
-export interface UserRepository {
-  findByEmail(email: string): Promise<User | undefined>;
-}
-
-export interface PasswordCredentials {
-  email: string;
-  password: string;
-}
 
 export interface LoginRequest extends HttpRequest {
   body: {
