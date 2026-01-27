c181143e99271d28c485d81d32579331b609d264
Dhemy <imdhemy@gmail.com>
2026-01-27 09:39:23 +0100

    wip: fix AccessToken & User imports

--- src/authentication/infrastructure/access-token/generate-access-token.ts
+++ src/authentication/infrastructure/access-token/generate-access-token.ts
@@ -1,8 +1,8 @@
-import { type AccessToken } from '@/authentication';
+import { type AccessToken } from '@/authentication/domain/access-token';
 import { sign } from '@/authentication/infrastructure/access-token/jose-jwt';
 import { type JwtSigner } from '@/authentication/infrastructure/access-token/types';
 import { securityConfig } from '@/config';
-import { type User } from '@/shared/domain';
+import { type User } from '@/shared/domain/entity/User';
 
 export async function generateAccessToken(user: User, signJwt: JwtSigner = sign): Promise<AccessToken> {
   return {
