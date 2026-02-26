4f7dc83e0ca8e1632b1245a9d9a930450805ff7c
Dhemy <imdhemy@gmail.com>
2026-02-26 12:30:34 +0100

    chore(setup): implement super admin setup repository

---
+++ src/app/shared/infrastructure/persistence/repository/user-repository/exists-super-admin.test.ts
@@ -0,0 +1,30 @@
+import { integrationTest } from '@tests/__vitest__/integration-test';
+import { describe, expect, test } from 'vitest';
+import { createExistsSuperAdmin } from '@/app/shared/infrastructure/persistence/repository/user-repository/exists-super-admin';
+import { usersCollection } from '@/composition/persistence/mongodb';
+
+describe('Exists Super Admin Repository', () => {
+  integrationTest();
+
+  test('returns false when super admin does not exist', async () => {
+    const existsSuperAdmin = createExistsSuperAdmin({ usersCollection });
+
+    const result = await existsSuperAdmin();
+
+    expect(result).toBe(false);
+  });
+
+  test('returns true when super admin exists', async () => {
+    await usersCollection.insertOne({
+      username: 'admin',
+      email: 'admin@example.com',
+      password: '$hashed-password',
+      roles: ['ROLE_SUPER_ADMIN'],
+    });
+    const existsSuperAdmin = createExistsSuperAdmin({ usersCollection });
+
+    const result = await existsSuperAdmin();
+
+    expect(result).toBe(true);
+  });
+});
