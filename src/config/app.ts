3a21d847c114195c47f22940028015b149b41474
Dhemy <imdhemy@gmail.com>
2026-04-27 17:09:41 +0200

    chore(routing): migrate to function-first routes (#115)

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,8 @@
 import { type KoalaConfig } from '@koala-ts/framework';
-import { AuthController } from '@/controller/AuthController';
-import { HomeController } from '@/controller/HomeController';
-import { WorkspaceController } from '@/controller/WorkspaceController';
+import { apiRoutes, homeRoute } from '@/routes';
 
 export const appConfig: KoalaConfig = {
-  controllers: [HomeController, AuthController, WorkspaceController],
+  controllers: [],
+  routes: [homeRoute, apiRoutes],
   globalMiddleware: [],
 };
