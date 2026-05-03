f2a9d0d3bfa31a9bfae38fd90fbe73b0c2eb5324
Dhemy <imdhemy@gmail.com>
2026-05-03 20:50:04 +0200

    chore: maintain routes and validation middleware

--- src/modules/authentication/interface/http/authentication-routes.ts
+++ src/modules/authentication/interface/http/authentication-routes.ts
@@ -1,13 +1,7 @@
-import { Route, RouteGroup } from '@koala-ts/framework/routing';
+import { Post, RouteGroup } from '@koala-ts/framework/routing';
 import { validateLoginRequest } from '@/modules/authentication/authentication-composition';
 import { loginHandler } from '@/modules/authentication/interface/http/login-handler';
 
 export const authenticationRoutes = RouteGroup({ namePrefix: 'auth.' }, () => [
-  Route({
-    method: 'POST',
-    path: '/login',
-    name: 'login',
-    middleware: [validateLoginRequest],
-    handler: loginHandler,
-  }),
+  Post('/login', 'login', validateLoginRequest, loginHandler),
 ]);
--- src/modules/workspace/interface/http/workspace-routes.ts
+++ src/modules/workspace/interface/http/workspace-routes.ts
@@ -1,13 +1,7 @@
-import { Route, RouteGroup } from '@koala-ts/framework/routing';
+import { Post, RouteGroup } from '@koala-ts/framework/routing';
 import { createWorkspaceHandler } from '@/modules/workspace/interface/http/create-workspace-handler';
 import { validateCreateWorkspaceRequest } from '@/modules/workspace/workspace-composition';
 
 export const workspaceRoutes = RouteGroup({ prefix: '/workspaces', namePrefix: 'workspaces.' }, () => [
-  Route({
-    method: 'POST',
-    path: '/',
-    name: 'create',
-    middleware: [validateCreateWorkspaceRequest],
-    handler: createWorkspaceHandler,
-  }),
+  Post('/', 'create', validateCreateWorkspaceRequest, createWorkspaceHandler),
 ]);
