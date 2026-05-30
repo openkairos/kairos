6444c40370ebce159065406c0fa9d4afa84e4846
Dhemy <imdhemy@gmail.com>
2026-05-30 19:53:52 +0200

    wip: rename types file

--- src/kairos/shared/logger/logger-composition.ts
+++ src/kairos/shared/logger/logger-composition.ts
@@ -1,6 +1,6 @@
-import { type Logger } from '@/kairos/shared/logger/logger';
-import { createLogger } from '@/kairos/shared/logger/pino';
 import { loggerConfig } from '@/config/logger';
+import { type Logger } from '@/kairos/shared/logger/logger.type';
+import { createLogger } from '@/kairos/shared/logger/pino';
 
 export const appLogger: Logger = createLogger({
   level: loggerConfig.level,
--- src/kairos/shared/logger/pino.ts
+++ src/kairos/shared/logger/pino.ts
@@ -1,5 +1,5 @@
+import { type Logger, type LoggerOptions } from '@/kairos/shared/logger/logger.type';
 import { type DestinationStream, type Logger as Pino, type LoggerOptions as PinoOptions, pino } from 'pino';
-import { type Logger, type LoggerOptions } from '@/kairos/shared/logger/logger';
 
 export function createLogger(options: LoggerOptions, stream?: DestinationStream): Logger {
   const logger = pino(toPinoOptions(options), stream);
