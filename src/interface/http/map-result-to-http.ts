ccc2f379ee25fd51232a6afa0e7a9af6cc9157c3
Dhemy <imdhemy@gmail.com>
2026-05-30 21:35:42 +0200

    wip: move normalizer up

--- src/interface/http/map-result-to-http.ts
+++ src/interface/http/map-result-to-http.ts
@@ -1,4 +1,4 @@
-import { normalize } from '@/framework/normalizer/normalize';
+import { normalize } from '@/framework/normalize';
 import { createResultToHttpMapper } from '@/interface/http/result-to-http';
 
 export const mapResultToHttp = createResultToHttpMapper({ normalize: normalize });
