66a6dfcff46bc795df5a4a23af8bbcdd04970186
Dhemy <imdhemy@gmail.com>
2026-05-30 21:55:48 +0200

    wip: simplify validation

--- src/framework/http/validator.ts
+++ src/framework/http/validator.ts
@@ -1,6 +1,5 @@
-import { type RequestValidator } from '@/interface/http/request-validator.type';
 import { builtInConstraints, createValidationMiddleware, createValidator } from '@koala-ts/framework/validator';
 
 const validate = createValidator({ constraints: builtInConstraints });
 
-export const validateRequest: RequestValidator = createValidationMiddleware({ validate });
+export const validateRequest = createValidationMiddleware({ validate });
--- src/interface/http/request-validator.type.ts
+++
@@ -1,4 +0,0 @@
-import { type HttpMiddleware } from '@koala-ts/framework';
-import { type ValidationSchema } from '@koala-ts/framework/validator';
-
-export type RequestValidator = (schema: ValidationSchema) => HttpMiddleware;
