edf158ff79114b4c87b405bcf905e8486ac6a109
Dhemy <imdhemy@gmail.com>
2026-05-31 17:32:12 +0200

    wip: add create workspace command

--- src/kairos/workspace/create-workspace/create-workspace.ts
+++ src/kairos/workspace/create-workspace/create-workspace.ts
@@ -4,12 +4,24 @@ import type { WorkspaceSlugConflictError } from '@/kairos/workspace/domain/error
 import { initializeWorkspace, type InitializeWorkspaceProps } from '@/kairos/workspace/domain/workspace';
 import type { Workspace } from '@/kairos/workspace/domain/workspace';
 
+export type CreateWorkspaceCommand = Readonly<{
+  environments?: string[];
+  name: string;
+  slug: string;
+}>;
+
 export type CreateWorkspaceResult = Result<Workspace, WorkspaceSlugConflictError>;
+
+const toInitializeWorkspaceProps = (command: CreateWorkspaceCommand): InitializeWorkspaceProps => ({
+  environments: command.environments,
+  name: command.name,
+  slug: command.slug,
+});
 
 export const makeCreateWorkspace =
   (insertWorkspace: InsertWorkspace) =>
-  async (props: InitializeWorkspaceProps): Promise<CreateWorkspaceResult> => {
-    const workspace = initializeWorkspace(props);
+  async (command: CreateWorkspaceCommand): Promise<CreateWorkspaceResult> => {
+    const workspace = initializeWorkspace(toInitializeWorkspaceProps(command));
 
     return insertWorkspace(workspace);
   };
