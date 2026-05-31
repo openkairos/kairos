18ed6dfdf0c7089540c84402a2aea6009b03ba8a
Dhemy <imdhemy@gmail.com>
2026-05-31 16:01:06 +0200

    wip: inline validation

--- src/kairos/workspace/interface/http/workspace-routes.ts
+++ src/kairos/workspace/interface/http/workspace-routes.ts
@@ -1,7 +1,8 @@
+import { validateRequest } from '@/framework/http/validator';
 import { createWorkspaceController } from '@/kairos/workspace/interface/http/create-workspace-controller';
-import { validateCreateWorkspaceRequest } from '@/kairos/workspace/workspace-composition';
+import { createWorkspaceRules } from '@/kairos/workspace/interface/http/create-workspace-request';
 import { Post, RouteGroup } from '@koala-ts/framework/routing';
 
 export const workspaceRoutes = RouteGroup({ prefix: '/workspaces', namePrefix: 'workspaces.' }, () => [
-  Post('/', 'create', validateCreateWorkspaceRequest, createWorkspaceController),
+  Post('/', 'create', validateRequest(createWorkspaceRules), createWorkspaceController),
 ]);
--- src/kairos/workspace/workspace-composition.ts
+++
@@ -1,4 +0,0 @@
-import { validateRequest } from '@/framework/http/validator';
-import { createWorkspaceRequestConstraints } from '@/kairos/workspace/interface/http/create-workspace-request';
-
-export const validateCreateWorkspaceRequest = validateRequest(createWorkspaceRequestConstraints);
